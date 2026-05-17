244e8f550d8d060daf5fd06622ccc22b1f8f6607
andy12seijas <seijasandy5@gmail.com>
2026-05-17 14:48:43 -0400

    base de datoa

--- mysite/settings.py
+++ mysite/settings.py
@@ -87,14 +87,14 @@ TEMPLATES = [
 WSGI_APPLICATION = 'mysite.wsgi.application'
 
 # Database
-DATABASES = {
+"""DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-}
+}"""
 
-"""DATABASES = {
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'vtcodcnb_miapp_db',      # El nombre exacto con prefijo
@@ -106,7 +106,7 @@ DATABASES = {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
         },
     }
-}"""
+}
 
 
 # Password validation
